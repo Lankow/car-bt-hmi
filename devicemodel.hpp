c0088e0da0604cbff2ac46014a03a5f51a01fe52
Lankow <maciej.lankow@gmail.com>
2025-05-30 20:55:34 +0200

    Delete files.

--- devicemodel.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DEVICEMODEL_HPP
-#define DEVICEMODEL_HPP
-
-#include <QAbstractListModel>
-#include "DeviceInfo.hpp"
-
-class DeviceModel : public QAbstractListModel {};
-
-#endif // DEVICEMODEL_HPP
